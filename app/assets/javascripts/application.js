f4cf291609dafbc73f389ec4b801c9e9c8967433
Oliver Hoffman <oliverhoffmanx0@yahoo.com>
2015-09-24 00:15:17 -0400

    finished animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require d3
-//= require_tree .
 //= require uikit
+//= require_tree .
+
 
